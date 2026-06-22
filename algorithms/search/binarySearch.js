e8ac49912a757ae387c13774290868d4de7ff988
ManhND <nguyenduymanh080302@gmail.com>
2026-06-22 16:00:54 +0700

    algorithm/binary-search

---
+++ algorithms/search/binarySearch.js
@@ -0,0 +1,23 @@
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const value = array[mid];
+
+    if (value === target) {
+      return mid;
+    }
+
+    if (value < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+module.exports = binarySearch;
